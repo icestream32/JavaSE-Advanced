54c4d6a813b8f595a1b74bd618f90542b1e62581
icestream32 <1601334224@qq.com>
2022-01-23 18:19:20 +0800

    数字类

---
+++ 常用类/数字类/BigDeciamlTest.java
@@ -0,0 +1,9 @@
+package 常用类.数字类;
+/*
+    BigDecimal是一个大数据类，在此类下输入的数字精度极高，通常用于财务软件的数据类型
+    面试题：处理财务数据时，用什么数据类型，千万别答double，double精度不够，用BigDecimal
+    具体语法规则参考JDK帮助文档
+ */
+public class BigDeciamlTest {
+    
+}
---
+++ 常用类/数字类/DecimalFormatTest.java
@@ -0,0 +1,8 @@
+package 常用类.数字类;
+// DecimalFormat专门负责数字格式化的一个类
+// 语法：DecimalFormat df = new DecimalFormat(数字格式)
+// 详情请查阅JDK文档，位于java.text包下
+
+public class DecimalFormatTest {
+    
+}
