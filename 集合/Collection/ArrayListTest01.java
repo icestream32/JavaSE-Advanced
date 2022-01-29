38c5b2b7bc547c81741c8fc47923831797a967c0
icestream32 <1601334224@qq.com>
2022-01-29 17:36:24 +0800

    ArrayList集合

---
+++ 集合/Collection/ArrayListTest01.java
@@ -0,0 +1,54 @@
+package 集合.Collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ArrayList集合：
+    1.默认初始化容量10
+    2.集合底层是一个Object[]类数组。
+    3.构造方法：
+        new ArrayList();
+        new ArrayList(数组长度);
+    4.ArrayList集合的扩容：
+        增长到原容量的1.5被。
+        ArrayList集合底层就是数组，怎么优化？
+            尽可能少的扩容。因为数组扩容效率比较低
+            建议在使用ArrayList集合的时候预估元素的个数，给定一个初始化容量
+    5.数组优点：
+        检索效率比较高。
+    6.数组缺点：
+        随机增删元素效率比较低。
+    7.向数组末尾添加元素，效率很高，不受影响
+    8.面试官经常问的一个问题？
+        这么多的集合中，你用哪个集合最多？
+        答：ArrayList集合。
+        因为往数组末尾添加元素，效率不受影响
+        另外，我们检索/查找某个元素的操作比较多。        
+ */
+public class ArrayListTest01 {
+    public static void main(String[] args) {
+        
+        // 创建一个ArrayList对象,长度默认为10
+        List list = new ArrayList();
+        // 注意，size方法是当前集合中所含有元素的个数，而不是ArrayList的长度
+        // 往数组末尾添加元素效率比较高
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        // 数组扩容
+        list.add(11);
+        /* 扩容源码
+        数组容量会增大1.5倍
+        int newCapacity = ArraysSupport.newLength(oldCapacity,
+                    minCapacity - oldCapacity,oldCapacity >> 1); */
+
+    }
+}
---
+++ 集合/Collection/ArrayTest02.java
@@ -0,0 +1,30 @@
+package 集合.Collection;
+
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/*
+ArrayList特殊构造方法
+    ArrayList​(Collection<? extends E> c) 
+    按照集合的迭代器返回的顺序构造一个包含指定集合元素的列表。
+    可以将一个非ArrayList集合转换成ArrayList集合 
+ */
+public class ArrayTest02 {
+    public static void main(String[] args) {
+        
+        Collection s = new HashSet();
+        s.add(1);
+        s.add(2);
+        s.add(3);
+        s.add(4);
+        s.add(5);
+
+        // 转换成ArrayList集合
+        ArrayList s1 = new ArrayList(s);
+        for (int i = 0; i < s1.size(); i++) {
+            System.out.println(s1.get(i));
+        }
+
+    }   
+}
