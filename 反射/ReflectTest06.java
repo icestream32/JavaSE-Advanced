b41a47a19b9a40ad434f363f1d987c4939d287d1
icestream32 <1601334224@qq.com>
2022-02-06 16:13:57 +0800

    反射机制调用Method方法

--- 反射/ReflectTest06.java
+++ 反射/ReflectTest06.java
@@ -1,21 +1,24 @@
 package 反射;
 
-// import java.lang.reflect.Method;
+import java.lang.reflect.Constructor;
+
+import java.lang.reflect.Method;
 
 // 反射机制调用Method方法
 public class ReflectTest06 {
     public static void main(String[] args) throws Exception {
         
-        /* Class<?> methodClass = Class.forName("反射.bean.UserService");
+        Class<?> methodClass = Class.forName("反射.bean.UserService");
         // 新建obj对象，其实是UserService对象
-        Object obj = methodClass.newInstance();
+        Constructor<?> constructor = methodClass.getConstructor();
+        Object obj = constructor.newInstance();
         // 将login方法传到method对象中
         Method method = methodClass.getDeclaredMethod("login", String.class,String.class);
         // 调用方法
         // 利用反射机制调用方法时，方法的返回值都是Object类型
         Object retValue = method.invoke(obj, "admin","123");
 
-        System.out.println((boolean) retValue ? "登录成功!" : "登录失败！"); */
+        System.out.println((boolean) retValue ? "登录成功!" : "登录失败！");
 
     }
 }
